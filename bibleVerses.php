11a0e4af8ace34d781280d41ea42baa06b8d17f9
jaredgeddy <jaredgeddy@gmail.com>
2016-04-12 19:20:29 -0700

    added the init for plugin

---
+++ bibleVerses.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Bible Verses
+ * Description: A plugin that easily allows you to quote Bible verses in your posts
+ * Version: 1.0
+ * Author: Jared Eddy
+ * Author URI: http://www.sitesrealized.com
+ */
